a8711543b2746a50312f73e766a2fec6fb51b36f
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-01-02 16:25:11 +0900

    Add help

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,26 @@ const switchAccount = (name: string) => {
 }
 
 const help = () => {
-    console.log("help");
+    let text:string = "\x1b[1mCOMMANDS:\x1b[0m\n";
+
+    text += "  Add current account to switch target.\n";
+    text += "\x1b[34m    \"add <name>\"\x1b[0m or \x1b[34m\"-a <name>\"\x1b[0m\n";
+    text += "    <name> is used to identify the account.\n\n";
+
+    text += "  Delete the account named <name>.\n";
+    text += "\x1b[34m    \"delete <name>\"\x1b[0m or \x1b[34m\"-d <name>\"\x1b[0m\n";
+    text += "    Currently active accounts cannot be deleted.\n\n";
+
+    text += "  Switching to <name>.\n";
+    text += "\x1b[34m    \"switch <name>\"\x1b[0m or \x1b[34m\"-s <name>\"\x1b[0m\n\n";
+
+    text += "  Print this message.\n";
+    text += "\x1b[34m    \"help\"\x1b[0m or \x1b[34m\"-h\"\x1b[0m\n\n";
+
+    text += "\x1b[1mREPOSITORY:\x1b[0m\n"
+    text += "  https://github.com/rmuraix/clasp-switcher"
+
+    console.log(text);
 }
 
 const main = (argument: string) => {
